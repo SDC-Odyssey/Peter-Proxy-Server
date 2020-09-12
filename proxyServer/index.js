a47c17e3d66dab4abd09387bd3ac787e9c4d89fc
Peter Vu <pvu@stripe.com>
2020-09-11 22:29:50 -0700

    updated proxy and created a docker file

--- proxyServer/index.js
+++ proxyServer/index.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, '/proxyClient'), {
 //   res.end();
 // });
 
+// Check In Service Proxy Here (Port 3003)
 app.get('/pricing/:room_id', async (req, res) => {
   try {
     const roomId = req.params.room_id;
@@ -50,3 +51,122 @@ app.get('/availability/:room_id', async (req, res) => {
     res.end();
   }
 });
+
+// Room Service Proxy Here (Port 3002)
+app.get('/rooms', async (req, res) => {
+  try {
+    const response = await axios.get('http://127.0.0.1:3002/rooms');
+    const responseString = JSON.stringify(response.data);
+
+    res.status(200);
+    res.send(responseString);
+    res.end();
+  } catch {
+    res.sendStatus(404);
+    res.end();
+  }
+});
+
+app.get('/rooms/:roomId', async (req, res) => {
+  try {
+    const { roomId } = req.params;
+    const response = await axios.get(`http://127.0.0.1:3002/rooms/${roomId}`);
+    const responseString = JSON.stringify(response.data);
+    res.status(200);
+    res.send(responseString);
+    res.end();
+  } catch {
+    console.log('An error gets hit');
+    res.sendStatus(404);
+    res.end();
+  }
+});
+
+app.get('/rooms/:roomId/title', async (req, res) => {
+  try {
+    const { roomId } = req.params;
+    const response = await axios.get(`http://127.0.0.1:3002/rooms/${roomId}/title`);
+    const responseString = JSON.stringify(response.data);
+
+    res.status(200);
+    res.send(responseString);
+    res.end();
+  } catch {
+    res.sendStatus(404);
+    res.end();
+  }
+});
+
+app.get('/rooms/:roomId/overview', async (req, res) => {
+  try {
+    const { roomId } = req.params;
+    const response = await axios.get(`http://127.0.0.1:3002/rooms/${roomId}/overview`);
+    const responseString = JSON.stringify(response.data);
+
+    res.status(200);
+    res.send(responseString);
+    res.end();
+  } catch {
+    res.sendStatus(404);
+    res.end();
+  }
+});
+
+app.get('/rooms/:roomId/highlights', async (req, res) => {
+  try {
+    const { roomId } = req.params;
+    const response = await axios.get(`http://127.0.0.1:3002/rooms/${roomId}/highlights`);
+    const responseString = JSON.stringify(response.data);
+
+    res.status(200);
+    res.send(responseString);
+    res.end();
+  } catch {
+    res.sendStatus(404);
+    res.end();
+  }
+});
+
+app.get('/rooms/:roomId/description', async (req, res) => {
+  try {
+    const { roomId } = req.params;
+    const response = await axios.get(`http://127.0.0.1:3002/rooms/${roomId}/description`);
+    const responseString = JSON.stringify(response.data);
+
+    res.status(200);
+    res.send(responseString);
+    res.end();
+  } catch {
+    res.sendStatus(404);
+    res.end();
+  }
+});
+
+app.get('/rooms/:roomId/sleeping-arranges', async (req, res) => {
+  try {
+    const { roomId } = req.params;
+    const response = await axios.get(`http://127.0.0.1:3002/rooms/${roomId}/sleeping-arranges`);
+    const responseString = JSON.stringify(response.data);
+
+    res.status(200);
+    res.send(responseString);
+    res.end();
+  } catch {
+    res.sendStatus(404);
+    res.end();
+  }
+});
+
+// app.post('/rooms', async (req, res) => {
+//   try {
+//     const response = await axios.post(`http://127.0.0.1:3002`);
+//     const responseString = JSON.stringify(response.data);
+
+//     res.status(200);
+//     res.send(responseString);
+//     res.end();
+//   } catch {
+//     res.sendStatus(404);
+//     res.end();
+//   }
+// });
